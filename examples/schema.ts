7d8108f355937b039fb2ebd939911a0c7a280f42
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2022-01-02 14:11:49 +0000

    DB Update

--- examples/schema.ts
+++ examples/schema.ts
@@ -10,4 +10,28 @@ export const schema = buildSchema(`
 type Query {
     hello: String
 }
-type
+type Subscription {
+    countDown: Int
+}
+`);
+
+export const roots = {
+  Query: {
+    hello: () => 'Hello World!',
+  },
+  subscription: {
+    /* eslint no-await-in-loop: "off" */
+
+    countDown: async function* fiveToOne() {
+      for (const number of [5, 4, 3, 2, 1]) {
+        await sleep(1000); // slow down a bit so user can see the count down on GraphiQL
+        yield { countDown: number };
+      }
+    },
+  },
+};
+
+export const rootValue = {
+  hello: roots.Query.hello,
+  countDown: roots.subscription.countDown,
+};
