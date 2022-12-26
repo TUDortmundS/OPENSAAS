9a217667a286b6b5ea791395a0e7df2a6637c531
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2022-12-26 21:08:07 +0000

    Update ProjectEditor.cs

--- resources/checkgit.sh
+++ resources/checkgit.sh
@@ -8,4 +8,23 @@ trap "exit 1" ERR
 # and including any changes on main.
 #
 
-# First fetch to ensure git is up to da
+# First fetch to ensure git is up to date. Fail-fast if this fails.
+git fetch;
+if [[ $? -ne 0 ]]; then exit 1; fi;
+
+# Extract useful information.
+GIT_BRANCH=$(git branch -v 2> /dev/null | sed '/^[^*]/d');
+GIT_BRANCH_NAME=$(echo "$GIT_BRANCH" | sed 's/* \([A-Za-z0-9_\-]*\).*/\1/');
+GIT_BRANCH_SYNC=$(echo "$GIT_BRANCH" | sed 's/* [^[]*.\([^]]*\).*/\1/');
+
+# Check if main is checked out
+if [ "$GIT_BRANCH_NAME" != "main" ]; then
+  read -p "Git not on main but $GIT_BRANCH_NAME. Continue? (y|N) " yn;
+  if [ "$yn" != "y" ]; then exit 1; fi;
+fi;
+
+# Check if branch is synced with remote
+if [ "$GIT_BRANCH_SYNC" != "" ]; then
+  read -p "Git not up to date but $GIT_BRANCH_SYNC. Continue? (y|N) " yn;
+  if [ "$yn" != "y" ]; then exit 1; fi;
+fi;
