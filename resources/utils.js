3c1b89df990a7dead764d3f796731abecf9631e1
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2022-12-29 14:07:10 +0000

    add debug log

--- resources/utils.js
+++ resources/utils.js
@@ -6,4 +6,45 @@ const path = require('path');
 const childProcess = require('child_process');
 
 function exec(command, options) {
-  const output = childPro
+  const output = childProcess.execSync(command, {
+    maxBuffer: 10 * 1024 * 1024, // 10MB
+    encoding: 'utf-8',
+    ...options,
+  });
+  return removeTrailingNewLine(output);
+}
+
+const childProcessExec = util.promisify(childProcess.exec);
+async function execAsync(command, options) {
+  const output = await childProcessExec(command, {
+    maxBuffer: 10 * 1024 * 1024, // 10MB
+    encoding: 'utf-8',
+    ...options,
+  });
+  return removeTrailingNewLine(output.stdout);
+}
+
+function removeTrailingNewLine(str) {
+  if (str == null) {
+    return str;
+  }
+
+  return str.split('\n').slice(0, -1).join('\n');
+}
+
+function rmdirRecursive(dirPath) {
+  if (fs.existsSync(dirPath)) {
+    for (const dirent of fs.readdirSync(dirPath, { withFileTypes: true })) {
+      const fullPath = path.join(dirPath, dirent.name);
+      if (dirent.isDirectory()) {
+        rmdirRecursive(fullPath);
+      } else {
+        fs.unlinkSync(fullPath);
+      }
+    }
+    fs.rmdirSync(dirPath);
+  }
+}
+
+function readdirRecursive(dirPath, opts = {}) {
+  const
