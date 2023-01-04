dc55f9b0e03ef72137db5792f2bf9dfc71fbf509
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2023-01-04 13:24:35 +0000

    Added Github Sponsors

--- src/__tests__/http-test.ts
+++ src/__tests__/http-test.ts
@@ -97,4 +97,50 @@ const QueryRootType = new GraphQLObjectType({
     thrower: {
       type: GraphQLString,
       resolve: () => {
-        throw new
+        throw new Error('Throws!');
+      },
+    },
+  },
+});
+
+const TestSchema = new GraphQLSchema({
+  query: QueryRootType,
+  mutation: new GraphQLObjectType({
+    name: 'MutationRoot',
+    fields: {
+      writeTest: {
+        type: QueryRootType,
+        resolve: () => ({}),
+      },
+    },
+  }),
+});
+
+function stringifyURLParams(urlParams?: { [param: string]: string }): string {
+  return new URLSearchParams(urlParams).toString();
+}
+
+function urlString(urlParams?: { [param: string]: string }): string {
+  let string = '/graphql';
+  if (urlParams) {
+    string += '?' + stringifyURLParams(urlParams);
+  }
+  return string;
+}
+
+function server<StateT = Koa.DefaultState, ContextT = Koa.DefaultContext>() {
+  const app = new Koa<StateT, ContextT>();
+
+  /* istanbul ignore next Error handler added only for debugging failed tests */
+  app.on('error', (error) => {
+    // eslint-disable-next-line no-console
+    console.warn('App encountered an error:', error);
+  });
+
+  return app;
+}
+
+describe('GraphQL-HTTP tests', () => {
+  describe('GET functionality', () => {
+    it('allows GET with query param', async () => {
+      const app = server(
