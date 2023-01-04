7abab3ff725c627f4370555ebaf676871b7d5707
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2023-01-04 08:49:02 +0000

    Update babel monorepo to v7.10.1

--- src/__tests__/http-test.ts
+++ src/__tests__/http-test.ts
@@ -12,4 +12,49 @@ import type { ASTVisitor, ValidationContext } from 'graphql';
 import sinon from 'sinon';
 import multer from 'multer';
 import { expect } from 'chai';
-import 
+import { describe, it } from 'mocha';
+import {
+  Source,
+  GraphQLError,
+  GraphQLString,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+  parse,
+  execute,
+  validate,
+  buildSchema,
+} from 'graphql';
+
+import { graphqlHTTP } from '../index';
+
+import multerWrapper from './helpers/koa-multer';
+
+declare module 'koa' {
+  interface Request {
+    body?: any;
+    rawBody: string;
+  }
+}
+
+type MulterFile = {
+  /** Name of the form field associated with this file. */
+  fieldname: string;
+  /** Name of the file on the uploader's computer. */
+  originalname: string;
+  /**
+   * Value of the `Content-Transfer-Encoding` header for this file.
+   * @deprecated since July 2015
+   * @see RFC 7578, Section 4.7
+   */
+  encoding: string;
+  /** Value of the `Content-Type` header for this file. */
+  mimetype: string;
+  /** Size of the file in bytes. */
+  size: number;
+  /**
+   * A readable stream of this file. Only available to the `_handleFile`
+   * callback for custom `StorageEngine`s.
+   */
+  stream: Readable;
+  /** `DiskStorage` only
