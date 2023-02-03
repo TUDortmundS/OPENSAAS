813d4a3cbe0ed481705ebdf32b3c3ae7f50c3527
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2023-02-03 20:25:37 +0000

    Autogenerated .pot files in preparation for autoupdate of the actual .po language files

--- src/__tests__/usage-test.ts
+++ src/__tests__/usage-test.ts
@@ -33,4 +33,43 @@ describe('Useful errors when incorrectly used', () => {
       errors: [
         {
           message:
+            'GraphQL middleware option function must return an options object or a promise which will be resolved to an options object.',
+        },
+      ],
+    });
+  });
+
+  it('requires option factory function to return object or promise of object', async () => {
+    const app = new Koa();
+
+    app.use(
+      mount(
+        '/graphql',
+        // @ts-expect-error
+        graphqlHTTP(() => Promise.resolve(null)),
+      ),
+    );
+
+    const response = await request(app.listen()).get('/graphql?query={test}');
+
+    expect(response.status).to.equal(500);
+    expect(JSON.parse(response.text)).to.deep.equal({
+      errors: [
+        {
+          message:
+            'GraphQL middleware option function must return an options object or a promise which will be resolved to an options object.',
+        },
+      ],
+    });
+  });
+
+  it('requires option factory function to return object with schema', async () => {
+    const app = new Koa();
+
+    app.use(
+      mount(
+        '/graphql',
+        // @ts-expect-error
+        graphqlHTTP(() => ({})),
+      ),
    
