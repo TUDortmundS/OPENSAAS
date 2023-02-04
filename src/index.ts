90ff13cb7a0d1edd5508f6358e55d80f5782de97
TUDortmundS <99536304+TUDortmundS@users.noreply.github.com>
2023-02-04 15:30:23 +0000

    removing garbage

--- src/index.ts
+++ src/index.ts
@@ -23,4 +23,47 @@ import {
   getOperationAST,
   specifiedRules,
 } from 'graphql';
-import { getG
+import { getGraphQLParams } from 'express-graphql';
+
+import type { Context, Request, Response } from 'koa';
+
+import { renderGraphiQL } from './renderGraphiQL';
+import type { GraphiQLOptions, GraphiQLData } from './renderGraphiQL';
+
+type MaybePromise<T> = Promise<T> | T;
+
+/**
+ * Used to configure the graphqlHTTP middleware by providing a schema
+ * and other configuration options.
+ *
+ * Options can be provided as an Object, a Promise for an Object, or a Function
+ * that returns an Object or a Promise for an Object.
+ */
+export type Options =
+  | ((
+      request: Request,
+      response: Response,
+      ctx: Context,
+      params?: GraphQLParams,
+    ) => OptionsResult)
+  | OptionsResult;
+export type OptionsResult = MaybePromise<OptionsData>;
+
+export interface OptionsData {
+  /**
+   * A GraphQL schema from graphql-js.
+   */
+  schema: GraphQLSchema;
+
+  /**
+   * A value to pass as the context to this middleware.
+   */
+  context?: unknown;
+
+  /**
+   * An object to pass as the rootValue to the graphql() function.
+   */
+  rootValue?: unknown;
+
+  /**
+  
